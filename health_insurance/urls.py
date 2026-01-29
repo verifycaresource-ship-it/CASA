464bdddef7ff4ff0ec03576639fba31d24d97084
verifycaresource-ship-it <verifycaresource@gmail.com>
2026-01-29 20:45:47 +0300

    commit changes

--- health_insurance/urls.py
+++ health_insurance/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from accounts.views import dashboard  # main dashboard
+from django.conf.urls.static import static
 
 # =========================
 # 🌐 DRF Routers
